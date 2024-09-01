e8162194bdd7a4a78f9dffc6d7e9bf4c4d7ab0b4
Duyanhhackvip <duyanhtrinhnguyen94@gmail.com>
2024-09-01 12:05:41 +0700

    Add files via upload

---
+++ mod.js
@@ -0,0 +1,337 @@
+function goc1() {
+    h5gg.clearResults();
+    h5gg.searchNumber('909000005', 'I64', '0x100000000', '0x200000000');
+    alert('Mod By Duy Anh(' + h5gg.getResultsCount() + '🇻🇳)');
+}
+function goc2() {
+    h5gg.clearResults();
+    h5gg.searchNumber('909000003', 'I64', '0x100000000', '0x200000000');
+    alert('Mod By Duy Anh(' + h5gg.getResultsCount() + '🇻🇳)');
+}
+function goc3() {
+    h5gg.clearResults();
+    h5gg.searchNumber('909000004', 'I64', '0x100000000', '0x200000000');
+    alert('Mod By Duy Anh(' + h5gg.getResultsCount() + '🇻🇳)');
+}
+function goc4() {
+    h5gg.clearResults();
+    h5gg.searchNumber('914000001', 'I64', '0x100000000', '0x200000000');
+    alert('Mod By Duy Anh(' + h5gg.getResultsCount() + '🇻🇳)');
+}
+
+
+function xm8() {
+    h5gg.editAll('909000085', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function ak() {
+    h5gg.editAll('909000063', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function scar() {
+    h5gg.editAll('909000068', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function mp1() {
+    h5gg.editAll('909000075', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function m1014a() {
+    h5gg.editAll('909000081', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function famas() {
+    h5gg.editAll('909000090', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function ump() {
+    h5gg.editAll('909000098', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function ump() {
+    h5gg.editAll('909000098', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function mp2() {
+    h5gg.editAll('909040010', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function mp2() {
+    h5gg.editAll('909040010', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function mp5() {
+    h5gg.editAll('909033002', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function m1887() {
+    h5gg.editAll('909035007', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function m4a1() {
+    h5gg.editAll('909033001', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function an94() {
+    h5gg.editAll('909035012', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function namdam() {
+    h5gg.editAll('909037011', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function luc() {
+    h5gg.editAll('909038012', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function thomson() {
+    h5gg.editAll('909038010', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function m1014b() {
+    h5gg.editAll('909039011', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function m1014b() {
+    h5gg.editAll('909039011', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function muakiem() {
+    h5gg.editAll('909044015', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function groda() {
+    h5gg.editAll('909041005', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function chimgokien() {
+    h5gg.editAll('909042008', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function lv100() {
+    h5gg.editAll('909042007', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function hopthe() {
+    h5gg.editAll('909044005', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function ngoisanh() {
+    h5gg.editAll('909043002', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function cuoiech() {
+    h5gg.editAll('909045005', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function cuoiboncau() {
+    h5gg.editAll('909043003', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function caothust() {
+    h5gg.editAll('909041012', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function caothutc() {
+    h5gg.editAll('909041013', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function raicingboi() {
+    h5gg.editAll('909043009', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function parafal() {
+    h5gg.editAll('909045001', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function camco() {
+    h5gg.editAll('909000034', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function tp1() {
+    h5gg.editAll('914000002', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function tp2() {
+    h5gg.editAll('914000003', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+}
+function tp3() {
+    h5gg.editAll('914038001', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Đã Bật Mod'
+    });
+
+		}
