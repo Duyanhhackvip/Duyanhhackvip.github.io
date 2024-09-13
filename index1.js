3e78ea53fe42abdc989aa46b6022c1e69244e96c
Duyanhhackvip <duyanhtrinhnguyen94@gmail.com>
2024-09-13 13:28:53 +0700

    index1.js

--- index1.js
+++ index1.js
@@ -1,13 +1,15 @@
+
 function s1() {
     h5gg.clearResults();
     h5gg.searchNumber('4397530849764387586', 'I64', '0x100000000', '0x200000000');
+    alert('Tìm Speed(' + h5gg.getResultsCount() + '/1)');
 }
+
 
 
 function s2() {
     h5gg.editAll('4397530849764387586', 'I64');
     
-        
 
 }
 
@@ -17,7 +19,5 @@ function s2() {
 function s3() {
     h5gg.editAll('4397530849758414897', 'I64');
     
-        
 
 }
-
