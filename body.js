74d5ec8b02bf74695f0370e38401e9b3aaa1287a
Duyanhhackvip <duyanhtrinhnguyen94@gmail.com>
2024-10-04 21:12:45 +0700

    Add files via upload

---
+++ body.js
@@ -0,0 +1,14 @@
+   loopy()
+  
+  function loopy() {
+    var sWord = "";
+    while (sWord != "duyanh") { 
+      sWord = prompt("seach google hoặc safari https://bitly.li/m11Z để lấy key");
+      if (sWord !== "duyanh") {
+        alert("Key sai vui lòng seach google hoặc safari https://bitly.li/m11Z để lấy key");
+      }
+    }
+    alert("Menu Make By Duy Anh");
+  }
+
+ 
